84ed3b3910e0128feddd8cd4e360969199cff42f
Maheshi Anuradha <86712971+Maheshi-anu@users.noreply.github.com>
2023-08-03 22:08:52 +0530

    Update cv.js

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -8,3 +8,28 @@ cvBtn.addEventListener("click", () => {
   link.download = "CV-Maheshi-Anuradha.pdf"; // Change the filename as desired
   link.dispatchEvent(new MouseEvent("click"));
 });
+
+
+let ecoBtn = document.getElementById("download_ecopilot");
+
+const ecoUrl = "assets/case_studies/Ecopilot.pdf";
+
+ecoBtn.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.href = ecoUrl;
+  link.download = "Ecopilot Case Study.pdf"; // Change the filename as desired
+  link.dispatchEvent(new MouseEvent("click"));
+});
+
+
+
+let dojoBtn = document.getElementById("download_dojo");
+
+const dojoUrl = "assets/case_studies/Coderdojo.pdf";
+
+dojoBtn.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.href = dojoUrl;
+  link.download = "CoderDojo.lk Case Study.pdf"; // Change the filename as desired
+  link.dispatchEvent(new MouseEvent("click"));
+});
