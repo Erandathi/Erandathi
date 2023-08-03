a181906647e6fa4ed173f80146818fe7e807001f
Maheshi Anuradha <86712971+Maheshi-anu@users.noreply.github.com>
2023-08-04 00:12:49 +0530

    Add files via upload

---
+++ scripts/cvbutton.js
@@ -0,0 +1,74 @@
+var btn6 = document.querySelector(".btn-6");
+
+window.addEventListener("DOMContentLoaded", function () {
+  btn6.addEventListener("mouseenter", function (e) {
+    var parentOffset = btn6.getBoundingClientRect(),
+      relX = e.pageX - parentOffset.left,
+      relY = e.pageY - parentOffset.top;
+    btn6.querySelector("span").style.top = relY + "px";
+    btn6.querySelector("span").style.left = relX + "px";
+  });
+  btn6.addEventListener("mouseout", function (e) {
+    var parentOffset = btn6.getBoundingClientRect(),
+      relX = e.pageX - parentOffset.left,
+      relY = e.pageY - parentOffset.top;
+    btn6.querySelector("span").style.top = relY + "px";
+    btn6.querySelector("span").style.left = relX + "px";
+  });
+});
+
+// download cv
+btn6.addEventListener("click", () => {
+  // Create a new anchor element to download the file
+  const downloadLink = document.createElement("a");
+
+  // Set the file URL and name
+  downloadLink.href = "CV/CV_maheshi anuradha.pdf";
+  downloadLink.download = "CV-Maheshi Anuradha.pdf";
+
+  // Append the anchor element to the document body
+  document.body.appendChild(downloadLink);
+
+  // Trigger the click event on the anchor element
+  downloadLink.click();
+
+  // Remove the anchor element from the document body
+  document.body.removeChild(downloadLink);
+});
+
+document.getElementById("ecobtn").addEventListener("click", () => {
+  // Create a new anchor element to download the file
+  const downloadLink = document.createElement("a");
+
+  // Set the file URL and name
+  downloadLink.href = "case/Ecopilot.pdf";
+  downloadLink.download = "Ecopilot.pdf";
+
+  // Append the anchor element to the document body
+  document.body.appendChild(downloadLink);
+
+  // Trigger the click event on the anchor element
+  downloadLink.click();
+
+  // Remove the anchor element from the document body
+  document.body.removeChild(downloadLink);
+});
+
+document.getElementById("codobtn").addEventListener("click", () => {
+  // Create a new anchor element to download the file
+  const downloadLink = document.createElement("a");
+
+  // Set the file URL and name
+  downloadLink.href = "case/Coderdojo.pdf";
+  downloadLink.download = "CoderDojo.pdf";
+
+  // Append the anchor element to the document body
+  document.body.appendChild(downloadLink);
+
+  // Trigger the click event on the anchor element
+  downloadLink.click();
+
+  // Remove the anchor element from the document body
+  document.body.removeChild(downloadLink);
+});
+
